54215d745e31ce5b15288afbb3d5328ca8b8dbdc
VaSilii <morar-2018@bk.ru>
2023-04-02 22:34:55 +0300

    delete/node-modules/1

--- backend/models/user.js
+++ backend/models/user.js
@@ -6,7 +6,7 @@ const userSchema = new mongoose.Schema({
     type: String,
     minlength: 2,
     maxlength: 30,
-    default: 'Жак-ив-кустом',
+    default: 'Жак-ив-кусто',
   },
   about: {
     type: String,
