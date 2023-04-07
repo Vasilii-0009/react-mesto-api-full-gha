9a7e6fd37cc028d7b0685c2559bb511f357f9491
VaSilii <morar-2018@bk.ru>
2023-04-07 09:28:30 +0300

    fix/log

--- frontend/src/utils/Api.js
+++ frontend/src/utils/Api.js
@@ -11,7 +11,7 @@ const configApi = {
 
 const configAuthApi = {
 
-  // url: "http://localhost:3000",
+  //url: "http://localhost:3000",
   url: "https://bekend-mesto-api.nomoredomains.monster",
   headers: {
     "Content-Type": "application/json",
