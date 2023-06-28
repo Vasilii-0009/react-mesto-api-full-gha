38c10bcd560d8d9caf98f74d898d32f18bc625c8
Василий Морарь <vasili-morari@MacBook-Air-Vasilij.local>
2023-06-28 21:43:57 +0300

    изменил адрес для БД

--- backend/app.js
+++ backend/app.js
@@ -17,7 +17,7 @@ app.use('*', cors());
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 mongoose.set('strictQuery', true);
-mongoose.connect('mongodb://localhost:27017/mestodb', {});
+mongoose.connect('mongodb://127.0.0.1:27017/mestodb', {});
 app.use(requestLogger);
 
 app.use(router);
